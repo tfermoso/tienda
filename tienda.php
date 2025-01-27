18bd045eb3db98601c40268b004cc7536686945a
tfermoso <tomas.fermoso@gmail.com>
2025-01-27 19:50:03 +0100

    comprobamos session

--- tienda.php
+++ tienda.php
@@ -1,3 +1,13 @@
+<?php
+session_start();
+if (isset($_SESSION["username"])) {
+    $username = $_SESSION["username"];
+} else {
+    header("Location: index");
+    exit();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
