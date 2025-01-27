a379d1a1542b5799ff055e22214e70baab5fcb51
tfermoso <tomas.fermoso@gmail.com>
2025-01-27 19:10:35 +0100

    añadimos estilos a index.php

--- index.php
+++ index.php
@@ -34,10 +34,12 @@ if (isset($_POST["username"])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tienda</title>
+    <link rel="stylesheet" href="css/index.css">
 </head>
 
 <body>
     <form action="" method="post">
+        <h1>Iniciar sesión tienda</h1>
         <label for="username">Nombre de usuario</label>
         <input type="text" name="username" id="username" required placeholder="Username">
         <label for="password">Contraseña</label>
