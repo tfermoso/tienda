71dc2156f24b4fd358c52363bffce0b944f5550a
tfermoso <tomas.fermoso@gmail.com>
2025-01-27 20:12:09 +0100

    add tienda.css

--- tienda.php
+++ tienda.php
@@ -18,20 +18,24 @@ if (isset($_SESSION["username"])) {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
         integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/tienda.css">
 </head>
 
 <body>
-    <header>
-        <i class="fa-solid fa-store"></i>
-        <p><?php echo $_SESSION["username"]; ?></p>
-    </header>
-    <main>
-        <aside></aside>
-        <section>
+    <div class="container">
+        <header>
+            <i class="fa-solid fa-store"></i>
+            <p><?php echo $_SESSION["username"]; ?></p>
+        </header>
+        <main>
+            <aside></aside>
+            <section>
 
-        </section>
-    </main>
-    <footer></footer>
+            </section>
+        </main>
+        <footer></footer>
+    </div>
+
 </body>
 
 </html>
