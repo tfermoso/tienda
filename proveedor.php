23d9649637af4d33c4c653cdb4dab4c0e268888b
tfermoso <tomas.fermoso@gmail.com>
2025-01-29 16:01:48 +0100

    consulta recuperar proveedores

--- proveedor.php
+++ proveedor.php
@@ -4,6 +4,10 @@ if(! isset($_GET["id"])){
     exit();
 }
 include("partials/cabecera.php");
+$sql="SELECT P.id,P.nombre,P.web,D.id,D.calle,D.numero,D.comuna,D.ciudad
+,T.id,T.numero FROM tienda.proveedores P 
+left join tienda.direcciones D on P.id=D.proveedores_id
+left join tienda.telefonos T on T.idproveedores=P.id;"
 
 ?>
 <section>
